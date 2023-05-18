d114cb47524a150a6b1ce308c81000e5d9398e0b
lorenzo7377 <lorenzodavinci@hotmail.com>
2023-05-18 13:26:06 -0300

    Caminho
    Foi criado o retorno do caminho do algoritmo

--- dijkstra.py
+++ dijkstra.py
@@ -16,21 +16,22 @@ class Grafo:
         distancias = {vertice: float('inf') for vertice in self.graph}
         distancias[vertice_inicial] = 0
         heap = [(0, vertice_inicial)]
+        caminho = {vertice_inicial: [vertice_inicial]}
 
         while heap:
             distancia_atual, vertice_atual = heapq.heappop(heap)
 
             if vertice_atual == vertice_final:
-                return distancia_atual
+                return distancia_atual, caminho[vertice_atual]
 
             if distancia_atual > distancias[vertice_atual]:
                 continue
 
             for vizinho, peso in self.graph[vertice_atual]:
                 distancia = distancia_atual + peso
-
                 if distancia < distancias[vizinho]:
                     distancias[vizinho] = distancia
                     heapq.heappush(heap, (distancia, vizinho))
+                    caminho[vizinho] = caminho[vertice_atual] + [vizinho]
 
-        return float('inf')
+        return float('inf'), caminho[vertice_final]
--- main.py
+++ main.py
@@ -10,50 +10,63 @@ def limpar_terminal():
 limpar_terminal()
 
 grafo = Grafo()
-grafo.graph = {
-    1: {5: 13, 2: 25},
-    2: {1: 25, 3: 30},
-    3: {2: 30, 14: 9, 12: 9},
-    4: {5: 4.5},
-    5: {4: 4.5, 8: 7, 1: 13},
-    6: {7: 5},
-    7: {6: 5},
-    8: {14: 9, 9: 4, 5: 7, 15: 8},
-    9: {8: 4},
-    10: {12: 5.5, 11: 2, 16: 6, 13: 5},
-    11: {10: 2, 15: 12},
-    12: {10: 5.5, 3: 9},
-    13: {10: 5},
-    14: {8: 9, 3: 9},
-    15: {11: 12, 8: 8}
-}
+
+grafo.adicionar_aresta('Eldoria', 'Drakonheim', 13)
+grafo.adicionar_aresta('Eldoria', 'Mythosia', 25)
+grafo.adicionar_aresta('Mythosia', 'Eldoria', 25)
+grafo.adicionar_aresta('Mythosia', 'Avaloria', 30)
+grafo.adicionar_aresta('Avaloria', 'Mythosia', 30)
+grafo.adicionar_aresta('Avaloria', 'Vale Verdejante', 9)
+grafo.adicionar_aresta('Avaloria', 'Stormgate', 9)
+grafo.adicionar_aresta('Serendell', 'Drakonheim', 4.5)
+grafo.adicionar_aresta('Drakonheim', 'Serendell', 4.5)
+grafo.adicionar_aresta('Drakonheim', 'Celestia', 7)
+grafo.adicionar_aresta('Drakonheim', 'Eldoria', 13)
+grafo.adicionar_aresta('Sylvanor', 'Frostholm', 5)
+grafo.adicionar_aresta('Frostholm', 'Sylvanor', 5)
+grafo.adicionar_aresta('Celestia', 'Vale Verdejante', 9)
+grafo.adicionar_aresta('Celestia', 'Shadowfen', 4)
+grafo.adicionar_aresta('Celestia', 'Drakonheim', 7)
+grafo.adicionar_aresta('Celestia', 'Ironhold', 8)
+grafo.adicionar_aresta('Shadowfen', 'Celestia', 4)
+grafo.adicionar_aresta('Crystalis', 'Stormgate', 5.5)
+grafo.adicionar_aresta('Crystalis', 'Emberfall', 2)
+grafo.adicionar_aresta('Crystalis', 'Stormgate', 6)
+grafo.adicionar_aresta('Crystalis', 'Picos Radiantes', 5)
+grafo.adicionar_aresta('Emberfall', 'Crystalis', 2)
+grafo.adicionar_aresta('Emberfall', 'Ironhold', 12)
+grafo.adicionar_aresta('Stormgate', 'Crystalis', 5.5)
+grafo.adicionar_aresta('Stormgate', 'Avaloria', 9)
+grafo.adicionar_aresta('Picos Radiantes', 'Crystalis', 5)
+grafo.adicionar_aresta('Vale Verdejante', 'Celestia', 9)
+grafo.adicionar_aresta('Vale Verdejante', 'Avaloria', 9)
+grafo.adicionar_aresta('Ironhold', 'Emberfall', 12)
+grafo.adicionar_aresta('Ironhold', 'Celestia', 8)
+
+local_atual = 'Celestia'
+
+print("================================================================================================================================================================")
+print(f"Seja bem vindo, ó nobre herói! Eu sou seu parceiro de viagem, o mapa do Mago Dijkstra! meu objetivo é te ajudar e te dizer a menor distância\ne o caminho mais rápido até sua jornarda!")
+
 while True:
-    vertice_inicial = int(input('\n\n->Diga a cidade em que está: '))
+    print(f"Você está em {local_atual}, para onde quer ir, nobre guerreiro?")
+    vertice_final = input('->Diga a cidade para onde vai: ')
     
-    vertice_final = int(input('->Diga a cidade para onde vai: '))
-    
-    menor_distancia = grafo.dijkstra(vertice_inicial, vertice_final)
+    menor_distancia, caminho = grafo.dijkstra(local_atual, vertice_final)
 
     if(menor_distancia == float('inf')):
-        print(f"\n-->Não há caminho entre {vertice_inicial} e {vertice_final}.")
+        print(f"\n-->Não há caminho entre {local_atual} e {vertice_final}.")
 
     else:
-        print(f"\n-->A menor distância entre {vertice_inicial} e {vertice_final} é: {menor_distancia}Km.")
+        local_atual = vertice_final
+        print(f"\n-->A menor distância entre {local_atual} e {vertice_final} é: {menor_distancia}Km.")
+        caminho_formatado = ' -> '.join(caminho)
+        print(caminho_formatado)
 
-    saida = input("Deseja sair (S/N)?: ")
+    
+   
+    saida = input("\nDeseja sair (S/N)?: ")
     limpar_terminal()
     if saida.lower() == "s" :
         break
 
-'''
-n = int(input("Digite o número de arestas:"))
-for i in range(n):
-    v1 = input("valor do vértice 1: ")
-    v2 = input("valor do vértice 2: ")
-    p = input("valor do peso:")
-    grafo.adicionar_aresta(v1, v2, p)
-
-for vertice in grafo.graph:
-    print(f"{vertice}:")
-    for vizinho, peso in grafo.graph{vertice}:
-        print(f"  Vizinho: {vizinho} - Peso: {peso}")'''
