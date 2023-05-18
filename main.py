306d23ec9b7ebb752cb933edd1d8c1f84dde537b
lorenzo7377 <lorenzodavinci@hotmail.com>
2023-05-18 11:07:31 -0300

    Criação do grafo
    foi criado o grafo do mapa

--- main.py
+++ main.py
@@ -8,20 +8,30 @@ def limpar_terminal():
         os.system('clear')
 
 limpar_terminal()
+
 grafo = Grafo()
-grafo.adicionar_aresta('A', 'B', 2)
-grafo.adicionar_aresta('A', 'C', 4)
-grafo.adicionar_aresta('B', 'C', 3)
-grafo.adicionar_aresta('D', 'E', 3)
-
+grafo.graph = {
+    1: {5: 13, 2: 25},
+    2: {1: 25, 3: 30},
+    3: {2: 30, 14: 9, 12: 9},
+    4: {5: 4.5},
+    5: {4: 4.5, 8: 7, 1: 13},
+    6: {7: 5},
+    7: {6: 5},
+    8: {14: 9, 9: 4, 5: 7, 15: 8},
+    9: {8: 4},
+    10: {12: 5.5, 11: 2, 16: 6, 13: 5},
+    11: {10: 2, 15: 12},
+    12: {10: 5.5, 3: 9},
+    13: {10: 5},
+    14: {8: 9, 3: 9},
+    15: {11: 12, 8: 8}
+}
 while True:
-    vertice_inicial = input('\n\n->Diga a cidade em que está: ')
+    vertice_inicial = int(input('\n\n->Diga a cidade em que está: '))
     
-
-    vertice_final = input('->Diga a cidade para onde vai: ')
-
+    vertice_final = int(input('->Diga a cidade para onde vai: '))
     
-
     menor_distancia = grafo.dijkstra(vertice_inicial, vertice_final)
 
     if(menor_distancia == float('inf')):
@@ -45,5 +55,5 @@ for i in range(n):
 
 for vertice in grafo.graph:
     print(f"{vertice}:")
-    for vizinho, peso in grafo.graph[vertice]:
+    for vizinho, peso in grafo.graph{vertice}:
         print(f"  Vizinho: {vizinho} - Peso: {peso}")'''
